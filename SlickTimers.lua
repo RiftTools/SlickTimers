bf890d14ff07db22f894b01842f2e9642f16c3a6
Priche <xavia75+rift@gmail.com>
2011-06-11 21:58:23 -0400

    Added feature to hide/not hide timers after completion

--- SlickTimers.lua
+++ SlickTimers.lua
@@ -6,13 +6,23 @@ if not Libra then
 	return
 end
 
-SlickTimers = {}
-
 local MainGUI    = SlickTimers_MainGUI
 local OptionsGUI = SlickTimers_OptionsGUI
 
 local TimerList = {}
 
+SlickTimers_Config = {
+	x = 500,
+	y = 500,
+	width = 220,
+	hide_when_done = true
+}
+
+--
+-- Add a timer
+--
+-- @param   String   name      Name of the timer, also the text to be displayed
+-- @param   int      duration  Duration of the timer in seconds
 local function AddTimer(name, duration)
 	local timer_bar = Libra.UI.ProgressBar:Create(MainGUI.Timers)
 	timer_bar.timer = Libra.Utils.Timer:Create(name, duration)
@@ -21,6 +31,9 @@ local function AddTimer(name, duration)
 	table.insert(TimerList, timer_bar)
 end
 
+--
+-- Refreshes the timer bars
+--
 local function Refresh()
 	table.sort(TimerList, function (a, b)		 
 	    return a.timer.remaining > b.timer.remaining
@@ -49,19 +62,30 @@ end
 ---------------------------------
 -- MainGUI Functions
 ---------------------------------
+
+--
+-- Control Box 'Options' button Click event
+--
 function MainGUI.controlbox.options.Event:LeftDown()
 	MainGUI:ShowSettings()
 end
 
+--
+-- Control Box 'New' button Click event
+--
 function MainGUI.controlbox.new.Event:LeftDown()
 	print('GUI Coming Soon - Add via command line:')
 	print('/slicktimers add 60 My Custom Timer')
 end
 
+--
+-- Updates then displays the OptionsGUI
+--
 function MainGUI:ShowSettings()	
 	OptionsGUI.form.barwidth:SetValue(SlickTimers_Config.width)
 	OptionsGUI.form.window_x:SetValue(SlickTimers_Config.x)
 	OptionsGUI.form.window_y:SetValue(SlickTimers_Config.y)
+	OptionsGUI.form.hide_when_done:Toggle(SlickTimers_Config.hide_when_done)
 	OptionsGUI:Show()
 end
 
@@ -72,14 +96,20 @@ function OptionsGUI.form.bt_Save.Event:LeftDown()
 	SlickTimers_Config.width = OptionsGUI.form.barwidth:GetValue()
 	SlickTimers_Config.x = OptionsGUI.form.window_x:GetValue()
 	SlickTimers_Config.y = OptionsGUI.form.window_y:GetValue()
+	SlickTimers_Config.hide_when_done = OptionsGUI.form.hide_when_done:GetValue()
 	Refresh()
 end
 
+--
+-- This handles checking our timers
+--
 local function _RefreshService()
 	for i, bar in pairs(TimerList) do
 		if bar.timer.remaining <= 0 then
-			table.remove(TimerList, i)
-			bar:SetVisible(false)
+			if SlickTimers_Config.hide_when_done then
+				table.remove(TimerList, i)
+				bar:SetVisible(false)
+			end
 		else
 			bar:SetValue(bar.timer.remaining)
 		end
